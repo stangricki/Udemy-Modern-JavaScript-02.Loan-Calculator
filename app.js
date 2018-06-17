d6df2fb914c3c928c1b29b37616f5911e2a1f926
Filip Stangricki <stangricki@gmail.com>
2018-06-17 10:29:27 +0200

    Added Loader

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const interestInput = document.getElementById('interest');
 const yearsToRepayInput = document.getElementById('years');
 
 // DISPLAY ELEMENTS
+const results = document.getElementById('results');
 const showMonthlyPayment = document.getElementById('monthly-payment');
 const showTotalPayment = document.getElementById('total-payment');
 const showTotalInterest = document.getElementById('total-interest');
+
+// LOADER ELEMENT
+const loader = document.getElementById('loading');
 
 
 function showError(){ // Creates & appends ERROR BUTTON
@@ -18,7 +22,7 @@ function showError(){ // Creates & appends ERROR BUTTON
 	errorButton.innerHTML = "Please check your numbers";
 	errorButton.disabled = true;
 	card.insertBefore(errorButton, heading);
-	setTimeout(removeErrorInfo, 2000);
+	setTimeout(removeErrorInfo, 3000);
 }
 
 function removeErrorInfo(){
@@ -26,9 +30,8 @@ function removeErrorInfo(){
 }
 
 
-function calculateResults(e){
-	e.preventDefault();
-
+function calculateResults(){
+	
 	const principal = parseFloat(amountInput.value);
 	const calculatedInterest = parseFloat(interestInput.value) / 100 / 12;
 	const calculatedPayments = parseFloat(yearsToRepayInput.value) * 12
@@ -42,11 +45,25 @@ function calculateResults(e){
 		showMonthlyPayment.value = monthly.toFixed(2);
 		showTotalPayment.value = (monthly * calculatedPayments).toFixed(2); 
 		showTotalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+		// Show results
+		results.style.display = 'block';
+
+		// Hide Loader
+		loader.style.display = 'none';
 	} else {
+		loader.style.display = 'none';
 		showError();
 	}
 
 }
 
 // LISTEN FOR SUBMIT
-loanForm.addEventListener('submit', calculateResults);
+loanForm.addEventListener('submit', function(e){
+	e.preventDefault();
+	// Hide results
+	results.style.display = 'none';
+
+	// Show Loader
+	loader.style.display = 'block';
+	setTimeout(calculateResults, 1500);
+});
